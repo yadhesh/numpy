977fc8da468881916fba39339c5bff39bcb64126
DG Yadhesh <r161246@rguktrkv.ac.in>
2019-12-21 18:12:44 +0530

    Append any two arrays

---
+++ dsp-lab/appendAnyArrays.py
@@ -0,0 +1,9 @@
+# Python3 code to demonstrate list 
+# concatenation using naive method 
+
+# Initializing lists 
+import numpy as n
+list1 = n.array(input('enter the elements of list 1:'))
+list2 = n.array(input('enter the elements of list 2:'))
+list3 = n.append(list1,list2)
+print "Concatenated list using naive method :",list3
